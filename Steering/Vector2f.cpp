14b0dbe87a275543f83f7ee9ea215268b7023527
Ben Reynolds <benr420@gmail.com>
2016-10-10 21:28:17 +0100

    Added Vector2f::Magnitude & Vector2f::Normalize

--- Steering/Vector2f.cpp
+++ Steering/Vector2f.cpp
@@ -21,6 +21,17 @@ Vector2f Vector2f::Multiply(Vector2f _V1, GLfloat _Scalar) {
 	return Vector2f(_V1.X * _Scalar, _V1.Y * _Scalar);
 }
 
+Vector2f Vector2f::Normalize(Vector2f _V1) {
+	GLfloat M = Magnitude(_V1);
+	return Vector2f(_V1.X / M, _V1.Y / M);
+}
+
+GLfloat Vector2f::Magnitude(Vector2f _V1) {
+	return static_cast<GLfloat>(sqrt(_V1.X * _V1.X + _V1.Y * _V1.Y));
+}
+
 Vector2f Vector2f::Zero() {
 	return Vector2f(0, 0);
 }
+
+
--- Steering/Vector2f.h
+++ Steering/Vector2f.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <glut.h>
+#include <math.h>
 
 class Vector2f {
 
@@ -13,7 +14,10 @@ public:
 	static Vector2f Subtract(Vector2f _V1, Vector2f _V2);
 	static Vector2f Multiply(Vector2f _V1, Vector2f _V2);
 	static Vector2f Multiply(Vector2f _V1, GLfloat _Scalar);
+	static Vector2f Normalize(Vector2f _V1);
 	static Vector2f Zero();
+
+	static GLfloat Magnitude(Vector2f _V1);
 
 };
 
