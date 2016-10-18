ee5e9f32a6b48e304c722d3c579964d7b554c911
Ben Reynolds <benr420@gmail.com>
2016-10-18 18:55:54 +0100

    Added Vector Magnitude/Multiplication

--- Steering/AIFramework/Vector.cpp
+++ Steering/AIFramework/Vector.cpp
@@ -14,6 +14,18 @@ sf::Vector2f Vector::Limit_Magnitude(sf::Vector2f _V, float _Max) {
 	return _V;
 }
 
+sf::Vector2f Vector::Multiply(sf::Vector2f _V, int _Scalar) {
+	return sf::Vector2f(_V.x * _Scalar, _V.y * _Scalar);
+}
+
+sf::Vector2f Vector::Multiply(sf::Vector2f _V1, sf::Vector2f _V2) {
+	return sf::Vector2f(_V1.x * _V2.x, _V1.y * _V2.y);
+}
+
 sf::Vector2f Vector::Zero() {
 	return sf::Vector2f(0, 0);
+}
+
+float Vector::Magnitude(sf::Vector2f _V) {
+	return sqrt(_V.x * _V.x + _V.y * _V.y);
 }
--- Steering/AIFramework/Vector.h
+++ Steering/AIFramework/Vector.h
@@ -7,7 +7,11 @@ public:
 
 	static sf::Vector2f Normalize(sf::Vector2f _V);
 	static sf::Vector2f Limit_Magnitude(sf::Vector2f _V, float _Max);
+	static sf::Vector2f Multiply(sf::Vector2f _V, int _Scalar);
+	static sf::Vector2f Multiply(sf::Vector2f _V1, sf::Vector2f _V2);
 	static sf::Vector2f Zero();
+
+	static float Magnitude(sf::Vector2f _V);
 
 };
 
