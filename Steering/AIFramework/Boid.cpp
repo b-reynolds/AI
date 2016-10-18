5e1e92236c51a0649c97cf9f8e87f0ff160b5cda
Ben Reynolds <benr420@gmail.com>
2016-10-18 18:56:31 +0100

    Attempted Arrive/Pursue

--- Steering/AIFramework/Boid.cpp
+++ Steering/AIFramework/Boid.cpp
@@ -18,12 +18,38 @@ void Boid::Rotate_Towards(sf::Vector2f _Target) {
 
 void Boid::Think(float _Delta_Time) {
 
-	//Evade(Target);
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Num1)) {
+		State = States::StSeek;
+	}
+	else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Num2)) {
+		State = States::StPursue;
+	}
+	else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Num3)) {
+		State = States::StArrive;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num4)) {
+		State = States::StEvade;
+	}
 
-	Seek(Target);
+	switch(State) {
+		case States::StSeek:
+			Seek(Target);
+			break;
+		case States::StPursue:
+			Pursue(Target);
+			break;
+		case States::StArrive:
+			Arrive(Target);
+			break;
+		case States::StEvade:
+			Evade(Target);
+			break;
 
-	Velocity = (Vector::Normalize(Velocity + Acceleration) * 0.5f);
-	std::cout << Velocity.x << ", " << Velocity.y << std::endl;
+	}
+
+	Velocity = (Vector::Normalize(Velocity + Acceleration) * SPEED);
+
+	std::cout << "Velocity (" << Velocity.x << ", " << Velocity.y << ")" << std::endl;
 
 	Rotate_Towards(Velocity);
 	Move(Velocity);
@@ -32,9 +58,40 @@ void Boid::Think(float _Delta_Time) {
 
 }
 
+void Boid::Arrive(sf::Vector2f _Target) {
+
+	sf::Vector2f Desired_Velocity = _Target - getPosition();
+	float Distance = Vector::Magnitude(Desired_Velocity);
+	Desired_Velocity = Vector::Normalize(Desired_Velocity);
+
+	if(Distance < 100) {
+		float M = SPEED * (Distance / 100);
+		Desired_Velocity = Desired_Velocity * M;
+	}
+	else {
+		Desired_Velocity = Desired_Velocity * SPEED;
+	}
+
+	sf::Vector2f Steering_Force = Desired_Velocity - Velocity;
+
+	Steering_Force = Vector::Limit_Magnitude(Steering_Force, MAX_FORCE);
+
+	Apply_Force(Steering_Force);
+
+}
+
+void Boid::Pursue(sf::Vector2f _Target) {
+
+	float T = 3;
+	sf::Vector2f Desired_Velocity = Vector::Normalize(_Target - getPosition()) * T * SPEED;
+	sf::Vector2f Steering_Force = Desired_Velocity - Velocity;
+	Apply_Force(Steering_Force);
+
+}
+
 void Boid::Seek(sf::Vector2f _Target) {
 	
-	sf::Vector2f Desired_Velocity = Vector::Normalize(_Target - getPosition());
+	sf::Vector2f Desired_Velocity = Vector::Normalize(_Target - getPosition()) * SPEED;
 	sf::Vector2f Steering_Force = Desired_Velocity - Velocity;
 	Apply_Force(Steering_Force);
 
@@ -42,7 +99,7 @@ void Boid::Seek(sf::Vector2f _Target) {
 
 void Boid::Evade(sf::Vector2f _Target) {
 
-	sf::Vector2f Desired_Velocity = Vector::Normalize(getPosition() - Target);
+	sf::Vector2f Desired_Velocity = Vector::Normalize(getPosition() - Target) * SPEED;
 	sf::Vector2f Steering_Force = Desired_Velocity - Velocity;
 	Apply_Force(Steering_Force);
 	
--- Steering/AIFramework/Boid.h
+++ Steering/AIFramework/Boid.h
@@ -10,7 +10,11 @@ public:
 
 	sf::Vector2f Target, Acceleration;
 
-	const float SPEED = 0.25f;
+	enum States { StEvade, StSeek, StPursue, StArrive };
+	enum States State;
+
+	const float SPEED = 0.1f;
+	const float MAX_FORCE = 0.1f;
 
 	void Apply_Force(sf::Vector2f _Force);
 	void Move(sf::Vector2f _Velocity);
@@ -18,6 +22,9 @@ public:
 	void Rotate_Towards(sf::Vector2f _Target);
 
 	void Think(float _Delta_Time);
+
+	void Arrive(sf::Vector2f _Target);
+	void Pursue(sf::Vector2f _Target);
 	void Seek(sf::Vector2f _Target);
 	void Evade(sf::Vector2f _Target);
 
