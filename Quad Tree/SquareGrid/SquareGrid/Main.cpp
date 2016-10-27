4835240df276f6a5609e9ceaf0330ef2c9fcb5a7
Ben Reynolds <benr420@gmail.com>
2016-10-27 10:42:42 +0100

    Create SFML project SquareGrid

---
+++ Quad Tree/SquareGrid/SquareGrid/Main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+
+	sf::RenderWindow window(sf::VideoMode(640, 640), "Square Grid");
+
+	while (window.isOpen())
+	{
+
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear(sf::Color::Black);
+
+		window.display();
+
+	}
+
+	return 0;
+}
