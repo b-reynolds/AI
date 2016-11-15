adfbc8f37a9c140112dc17fbb6e1020ca025ac56
Ben Reynolds <benr420@gmail.com>
2016-11-15 11:58:00 +0000

    Create PathFinder class

---
+++ Path Finding/SFML/AStarSFML/AStarSFML/PathFinder.cpp
@@ -0,0 +1,152 @@
+#include "PathFinder.h"
+
+void PathFinder::sortByLowestF(std::deque<Node>& deque) const
+{
+	LessThanByF sortMethod;
+	std::sort(deque.begin(), deque.end(), sortMethod);
+}
+
+int PathFinder::getManhattanDistance(const sf::Vector2i& start, const sf::Vector2i& end) const
+{
+	return abs(end.x - start.x) + abs(end.y - start.y);
+}
+
+int PathFinder::getMovementCost(const sf::Vector2i &start, const sf::Vector2i &end) const
+{
+	return start.x != end.x && start.y != end.y ? COST_DIAGONAL : COST_NONDIAGONAL;
+}
+
+std::vector<Node> PathFinder::getNeighbours(Node &node, Map &map)
+{
+	std::vector<Node> neighbours;
+
+	sf::Vector2i parentPos = node.getPosition();
+	sf::Vector2i nodePos(0, 0);
+
+	sf::Vector2i mapSize = map.getSize();
+
+	if (parentPos.y - 1 >= 0)
+	{
+		nodePos = sf::Vector2i(parentPos.x, parentPos.y - 1);
+		neighbours.push_back(Node(nodePos, map.getValue(nodePos), &node)); // North
+		if (parentPos.x + 1 < mapSize.x)
+		{
+			nodePos = sf::Vector2i(parentPos.x + 1, parentPos.y - 1);
+			neighbours.push_back(Node(nodePos, map.getValue(nodePos), &node)); // North East
+		}
+		if (parentPos.x - 1 >= 0)
+		{
+			nodePos = sf::Vector2i(parentPos.x - 1, parentPos.y - 1);
+			neighbours.push_back(Node(nodePos, map.getValue(nodePos), &node)); // North West
+		}
+	}
+
+	if (parentPos.x + 1 < mapSize.x)
+	{
+		nodePos = sf::Vector2i(parentPos.x + 1, parentPos.y);
+		neighbours.push_back(Node(nodePos, map.getValue(nodePos), &node)); // East
+	}
+
+	if (parentPos.x - 1 >= 0)
+	{
+		nodePos = sf::Vector2i(parentPos.x - 1, parentPos.y);
+		neighbours.push_back(Node(nodePos, map.getValue(nodePos), &node)); // West
+	}
+
+	if (parentPos.y + 1 < mapSize.y)
+	{
+		nodePos = sf::Vector2i(parentPos.x, parentPos.y + 1);
+		neighbours.push_back(Node(nodePos, map.getValue(nodePos), &node)); // South
+		if (parentPos.x + 1 < mapSize.x)
+		{
+			nodePos = sf::Vector2i(parentPos.x + 1, parentPos.y + 1);
+			neighbours.push_back(Node(nodePos, map.getValue(nodePos), &node)); // South East
+		}
+		if (parentPos.x - 1 >= 0)
+		{
+			nodePos = sf::Vector2i(parentPos.x - 1, parentPos.y + 1);
+			neighbours.push_back(Node(nodePos, map.getValue(nodePos), &node)); // South West
+		}
+	}
+
+	return neighbours;
+}
+
+bool PathFinder::isOnList(const Node &node, const std::deque<Node> &list)
+{
+	return std::find(list.begin(), list.end(), node) != list.end();
+}
+
+std::stack<sf::Vector2i> PathFinder::findPath(Map &map, const sf::Vector2i &start, const sf::Vector2i &end)
+{
+	std::stack<sf::Vector2i> path;
+	std::deque<Node> openList;
+	std::deque<Node> closedList;
+
+	if (start == end)
+	{
+		return path;
+	}
+
+	if (map.getValue(end) == 0)
+	{
+		return path;
+	}
+
+	openList.push_front(Node(start, map.getValue(start), nullptr));
+
+	while (openList.size() > 0)
+	{
+		sortByLowestF(openList);
+
+		Node* currentNode = new Node(openList.front());
+		openList.pop_front();
+		closedList.push_back(*currentNode);
+
+		if (currentNode->getPosition() == end)
+		{
+			break;
+		}
+
+		std::vector<Node> neighbours = getNeighbours(*currentNode, map);
+
+		for (auto& neighbour : neighbours)
+		{
+			if (!neighbour.isTraversable() || isOnList(neighbour, closedList))
+			{
+				continue;
+			}
+
+			if(isOnList(neighbour, openList))
+			{
+				
+			}
+			else
+			{
+				neighbour.setParent(currentNode);
+				neighbour.setH(getManhattanDistance(neighbour.getPosition(), end));
+				neighbour.setG(neighbour.getG() + getMovementCost(currentNode->getPosition(), neighbour.getPosition()));
+				openList.push_front(neighbour);
+				map.setValue(neighbour.getPosition(), map.TILE_VISITED);
+			}		
+		}
+	}
+
+	Node* pathNode = &closedList.back();
+	while (pathNode != nullptr)
+	{
+		path.push(pathNode->getPosition());
+		pathNode = pathNode->getParent();
+	}
+
+	return path;
+}
+
+PathFinder::PathFinder()
+{
+}
+
+
+PathFinder::~PathFinder()
+{
+}
---
+++ Path Finding/SFML/AStarSFML/AStarSFML/PathFinder.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <deque>
+#include <stack>
+#include "Node.h"
+#include "Map.h"
+
+struct LessThanByF
+{
+	bool operator() (const Node &n1, const Node &n2) const
+	{
+		return n1.getF() < n2.getF();
+	}
+};
+
+class PathFinder
+{
+
+private:
+
+	const int COST_DIAGONAL = 14;
+	const int COST_NONDIAGONAL = 10;
+
+	void sortByLowestF(std::deque<Node> &deque) const;
+
+	int getManhattanDistance(const sf::Vector2i &start, const sf::Vector2i &end) const;
+	int getMovementCost(const sf::Vector2i &start, const sf::Vector2i &end) const;
+	std::vector<Node> getNeighbours(Node &node, Map &map);
+
+	bool isOnList(const Node &node, const std::deque<Node>& list);
+
+public:
+
+	std::stack<sf::Vector2i> findPath(Map &map, const sf::Vector2i &start, const sf::Vector2i &end);
+
+	PathFinder();
+	~PathFinder();
+
+};
+
